4aeb4b73a5fe3e9ea74844666c8d3145af14bec7
coder <prajapati.rahul373@gmail.com>
2022-01-20 16:40:12 +0530

    fixed colapse issue on mobile view

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import "../styles/toggle.css";
 
 const Navbar = (props) => {
+  const buttonRef = useRef(null);
+
+  const handleToggle = () => {
+    if (
+      buttonRef.current.className === "navbar-toggler" &&
+      buttonRef.current.ariaExpanded === "true"
+    ) {
+      buttonRef.current.click();
+    }
+  };
   return (
     <div>
       <nav
@@ -23,6 +33,8 @@ const Navbar = (props) => {
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
+            id="#mobileNavButton"
+            ref={buttonRef}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -35,37 +47,49 @@ const Navbar = (props) => {
               </li>
 
               <li className="nav-item">
-                <Link className="nav-link" to="/business">
+                <Link
+                  className="nav-link"
+                  to="/business"
+                  onClick={handleToggle}
+                >
                   Business
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/entertainment">
+                <Link
+                  className="nav-link"
+                  to="/entertainment"
+                  onClick={handleToggle}
+                >
                   Entertainment
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/general">
+                <Link className="nav-link" to="/general" onClick={handleToggle}>
                   General
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/health">
+                <Link className="nav-link" to="/health" onClick={handleToggle}>
                   Health
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/science">
+                <Link className="nav-link" to="/science" onClick={handleToggle}>
                   Science
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/sports">
+                <Link className="nav-link" to="/sports" onClick={handleToggle}>
                   Sports
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/technology">
+                <Link
+                  className="nav-link"
+                  to="/technology"
+                  onClick={handleToggle}
+                >
                   Technology
                 </Link>
               </li>
